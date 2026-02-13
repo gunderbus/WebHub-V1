da7d6dab47ae4d44126dce947878bc101001c384
gunderbus <cole.gundermann@gmail.com>
2026-02-12 19:32:28 -0500

    added function

--- api/save.js
+++ api/save.js
@@ -2,6 +2,31 @@ import { MongoClient } from "mongodb";
 
 let cachedClient = null;
 
+async function savePassword(db, site, username, password) {
+  await db.collection("passwords").insertOne({
+    site,
+    username,
+    password,
+    createdAt: new Date(),
+  });
+}
+
+async function getPasswords(db) {
+  return await db.collection("passwords").find({}).toArray();
+}
+
+async function updatePassword(db, id, newPassword) {
+  await db.collection("passwords").updateOne(
+    { _id: id },
+    { $set: { password: newPassword, updatedAt: new Date() } }
+  );
+}
+
+async function deletePassword(db, id) {
+  await db.collection("passwords").deleteOne({ _id: id });
+}
+
+
 async function connectToDatabase() {
   if (cachedClient) return cachedClient;
 
